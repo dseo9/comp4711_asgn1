fd16200cbbad8d52b19103d165eaddd0a712941b
Elly Seo <ellyseo@ellys-mbp-2.wifi.bcit.ca>
2018-11-02 12:50:13 -0700

    successful in ranking score

--- public/Model/ranking.js
+++ public/Model/ranking.js
@@ -1,16 +1,39 @@
 rankings = document.getElementById("rankingBoard");
 
 //get database data as json 
-function getUserName(){
+function getData(){
     var jsonObj={};
     const data = firebase.database().ref().child('users/');
   
     data.on('value',snap => {
       jsonObj = JSON.stringify(snap.val(),null,2);
-    //   jsonObj = JSON.parse(jsonObj);
+      jsonObj = JSON.parse(jsonObj);
       console.log(jsonObj);
-      rankings.innerHTML = jsonObj;
+    //   rankings.innerHTML = jsonObj;
+    
+
+      var array = [];
+
+      for (var key in jsonObj) {
+          array.push(jsonObj[key]);
+        
+      }
+      array.sort(function(a, b){
+          return b.score - a.score;
+      });
+      
+      var rank = 1;
+      for (var i = 0; i < array.length; i++) {
+        if (i > 0 && array[i].score < array[i - 1].score) {
+            rank++;
+        }
+          array[i].rank = rank;
+      }
+      
+      console.log(array);
+    
+    
     });
   }
 
-  getUserName();
+  getData();

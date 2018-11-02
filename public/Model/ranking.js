298e0752c7b651da8a79301251f287e4c4196441
Elly Seo <ellyseo@ellys-mbp-2.wifi.bcit.ca>
2018-11-02 12:58:05 -0700

    verified ranking working with new user data added

--- public/Model/ranking.js
+++ public/Model/ranking.js
@@ -1,7 +1,7 @@
 rankings = document.getElementById("rankingBoard");
 
 //get database data as json 
-function getData(){
+function getDataAndRank(){
     var jsonObj={};
     const data = firebase.database().ref().child('users/');
   
@@ -9,31 +9,29 @@ function getData(){
       jsonObj = JSON.stringify(snap.val(),null,2);
       jsonObj = JSON.parse(jsonObj);
       console.log(jsonObj);
-    //   rankings.innerHTML = jsonObj;
     
-
       var array = [];
 
+      //push database data to array
       for (var key in jsonObj) {
-          array.push(jsonObj[key]);
-        
+          array.push(jsonObj[key]); 
       }
+
       array.sort(function(a, b){
           return b.score - a.score;
       });
       
       var rank = 1;
+
       for (var i = 0; i < array.length; i++) {
         if (i > 0 && array[i].score < array[i - 1].score) {
             rank++;
         }
           array[i].rank = rank;
       }
-      
       console.log(array);
-    
-    
+
     });
   }
 
-  getData();
+  getDataAndRank();
